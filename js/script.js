f158fb9d7f1c9341e675117192db1c87df5e8f41
thewhitehaven04 <thewhitehaven04@gmail.com>
2021-11-28 21:40:57 +0300

    implemented adding books to library

--- js/script.js
+++ js/script.js
@@ -26,47 +26,35 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-let leviathanWakes = new Book(
-  "Leviathan Wakes",
-  "James S.A. Corey",
-  577,
-  2011,
-  true
-);
-let abaddonsGate = new Book(
-  "Abaddon's Gate",
-  "James S.A. Corey",
-  547,
-  2013,
-  false
-);
-let cibolaBurn = new Book("Cibola Burn", "James S.A. Corey", 591, 2014, false);
+let leviathanWakes = new Book('Leviathan Wakes', 'James S.A. Corey', 577, 2011, true);
+let abaddonsGate = new Book("Abaddon's Gate", 'James S.A. Corey', 547, 2013, false);
+let cibolaBurn = new Book('Cibola Burn', 'James S.A. Corey', 591, 2014, false);
 
-[leviathanWakes, abaddonsGate, cibolaBurn].forEach((someBook) => {
+[leviathanWakes, abaddonsGate, cibolaBurn].forEach(someBook => {
   addBookToLibrary(someBook);
 });
 
 function createArticle(book) {
-  let bookArticle = document.createElement("article");
-  bookArticle.classList.add("bookshelf__book");
+  let bookArticle = document.createElement('article');
+  bookArticle.classList.add('bookshelf__book');
 
   const bookAttributeToDisplayName = {
-    title: "Title:",
-    author: "Author:",
-    pageCount: "Pages:",
-    yearOfPublishing: "Published:",
-    readStatus: "Already read:",
+    title: 'Title:',
+    author: 'Author:',
+    pageCount: 'Pages:',
+    yearOfPublishing: 'Published:',
+    readStatus: 'Already read:',
   };
 
   for (attribute in bookAttributeToDisplayName) {
-    const bookData = document.createElement("div");
-    bookData.classList.add("book__info-entry");
+    const bookData = document.createElement('div');
+    bookData.classList.add('book__info-entry');
 
-    const spanKey = document.createElement("span");
-    spanKey.style.width = "120px";
+    const spanKey = document.createElement('span');
+    spanKey.style.width = '120px';
     spanKey.textContent = bookAttributeToDisplayName[attribute];
 
-    const spanValue = document.createElement("span");
+    const spanValue = document.createElement('span');
     spanValue.textContent = book[attribute];
 
     bookData.appendChild(spanKey);
@@ -78,17 +66,44 @@ function createArticle(book) {
   return bookArticle;
 }
 
-const bookshelf = document.querySelector(".bookshelf");
+const bookshelf = document.querySelector('.bookshelf');
 
 for (let someBook of myLibrary) {
   article = createArticle(someBook);
   bookshelf.appendChild(article);
 }
 
-const button = document.querySelector(".bookshelf__add-new");
 
-button.addEventListener('click', (event) => {
-  const form = document.querySelector('.form-new-book');
+const buttonAddNew = document.querySelector('.bookshelf__add-new');
+buttonAddNew.addEventListener('click', event => {
+  const form = document.querySelector('.form');
+  form.classList.remove('form__disabled');
+});
 
-  form.classList.remove('form-new-book__disabled');
-})
+const buttonCancel = document.querySelector('.form-new-book__button_type_cancel');
+buttonCancel.addEventListener('click', event => {
+  const form = document.querySelector('.form');
+  form.classList.add('form__disabled');
+
+  // Cleanup input fields of the form
+  ['#title', '#author', '#page-count', '#year'].forEach(id => {
+    document.querySelector(id).textContent = '';
+  });
+  document.querySelector('#read-status').checked = false;
+});
+
+const buttonAccept = document.querySelector('.form-new-book__button_type_accept');
+/** The listener adds a new book to the library */
+buttonAccept.addEventListener('click', event => {
+  const args = ['#title', '#author', '#page-count', '#year'];
+  const book = new Book(
+    ...args.map(arg => document.querySelector(arg).value),
+    document.querySelector('#read-status').checked
+  );
+
+  addBookToLibrary(book);
+  
+  // Add an article
+  article = createArticle(book);
+  bookshelf.appendChild(article);
+});
