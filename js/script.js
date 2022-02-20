cdd177028df0ec9d49ffe8327b62ea92c4274e69
German <thewhitehaven04@gmail.com>
2022-02-20 20:51:20 +0300

    removal works

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-const bookshelf = document.querySelector('.bookshelf__main-pane');
+class Observable {
+  #observers = [];
 
-const myLibrary = {
-  books: [],
+  subscribe(observer) {
+    this.#observers.push(observer);
+  }
+
+  notify(data) {
+    for (let observer of this.#observers) {
+      observer(data);
+    }
+  }
+}
+class LibraryModel extends Observable {
+  /** Stores objects of the Book class. */
+  books = [];
 
   /** Adds a book to the library.
    * @param {Book} book - the book object to add to library.
@@ -9,15 +21,134 @@ const myLibrary = {
    */
   addBook(book) {
     this.books.push(book);
-    return this.books.length - 1;
-  },
+    this.notify({
+      event: 'add',
+      obj: book,
+    });
+  }
+
+  _getBookById(bookId) {
+    return this.books.find((book) => book.id == bookId);
+  }
+
   /** Removes a book from the library by its index.
-   * @param {Number} bookIndex - index of the book to be removed.
+   * @param {Book} book - index of the book to be removed.
    */
-  removeBook(bookIndex) {
-    this.books.splice(bookIndex, 1);
-  },
-};
+  removeBookById(bookId) {
+    const book = this._getBookById(bookId);
+    this.books.filter((libraryBook) => libraryBook == book);
+    this.notify({
+      event: 'remove',
+      obj: book,
+    });
+  }
+}
+
+class LibraryController {
+  constructor(view, model) {
+    this.libraryView = view;
+    this.libraryView.setupRemoveHandler(this.removeBook);
+
+    this.libraryModel = model;
+    this.libraryModel.subscribe(this.libraryView.updateDispatch);
+  }
+
+  addBook(book) {
+    this.libraryModel.addBook(book);
+  }
+
+  removeBook = (bookId) => {
+    this.libraryModel.removeBookById(bookId);
+  };
+}
+
+class LibraryView {
+  constructor() {
+    this.libraryContainer = document.querySelector('.bookshelf__main-pane');
+
+    this.bookClass = 'bookshelf__book';
+    this.bookAttributeClass = 'book__attribute';
+    this.bookRemoveButtonClass = 'book__button-remove';
+  }
+
+  setupRemoveHandler(removeHandler) {
+    this.libraryContainer.addEventListener('click', (event) => {
+      removeHandler(
+        this._getBookId(event.target.closest(`.${this.bookRemoveButtonClass}`)),
+      );
+    });
+  }
+
+  _getBookId(button) {
+    return button.dataset.id;
+  }
+
+  /** Implements reactions to events received from the model.
+   * @param {Object} object - object containing the 'event' and the 'obj' keys
+   */
+  updateDispatch = (object) => {
+    switch (object.event) {
+      case 'add':
+        this.addBook(object.obj);
+        break;
+      case 'remove':
+        this.removeBook(object.obj);
+        break;
+    }
+  };
+
+  addBook(book) {
+    const rootArticle = this._setupArticle(book);
+    const removeButton = this._setupButton(book.id);
+
+    rootArticle.appendChild(removeButton);
+    this.libraryContainer.appendChild(rootArticle);
+  }
+
+  removeBook(book) {
+    this.libraryContainer.removeChild(this._getBookFromDOM(book));
+  }
+
+  _getBookFromDOM(book) {
+    return this.libraryContainer.querySelector(`#${book.id}`);
+  }
+
+  _setupArticle(book) {
+    const rootArticle = document.createElement('article');
+    rootArticle.classList.add(this.bookClass);
+
+    for (let key in book)
+      rootArticle.appendChild(this._renderKeyValuePair(key, book[key]));
+
+    rootArticle.setAttribute('id', `${book.id}`);
+    return rootArticle;
+  }
+
+  _renderKeyValuePair(bookParameter, bookValue) {
+    const div = document.createElement('div');
+    div.classList.add(this.bookAttributeClass);
+
+    const spanKey = document.createElement('span');
+    spanKey.textContent = `${bookParameter}:`;
+
+    const spanValue = document.createElement('span');
+    spanKey.textContent = `${bookValue}`;
+
+    div.appendChild(spanKey);
+    div.appendChild(spanValue);
+    return div;
+  }
+
+  _setupButton(bookId) {
+    const button = document.createElement('button');
+    button.innerHTML = '<span class="material-icons-outlined">delete</span>';
+    button.dataset.id = bookId;
+
+    button.classList.add(this.bookRemoveButtonClass);
+
+    return button;
+  }
+}
 
 /** Constructs the Book object that has the following fields:
  * @param {String} title - title of the book;
@@ -26,150 +157,109 @@ const myLibrary = {
  * @param {Number} yearOfPublishing - the number of pages in the book;
  * @param {Boolean} readStatus - whether the book has been read.
  */
-function Book(title, author, pageCount, yearOfPublishing, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.yearOfPublishing = yearOfPublishing;
-  this.readStatus = readStatus;
-}
+class Book {
+  static nextBookId = 0;
+  #id;
 
-/** Toggles the read state of a book.
- * @param {Boolean} readStatus - the read status of the book.
- */
-Book.prototype.setRead = function (readStatus) {
-  this.readStatus = readStatus;
-};
+  constructor(title, author, pageCount, yearOfPublishing, readStatus) {
+    this.#id = `book-id-${Book.nextBookId}`;
+    Book.nextBookId++;
 
-/** Returns remove button element with a bookId property attached to it.
- * @param {Number} bookNumber - the library book index;
- * @param {Node} article - the article node
- * @returns {Element} button - the remove button element.
- */
-function createRemoveButton(bookNumber, article) {
-  const button = document.createElement('button');
-
-  button.classList.add('book__button-remove');
-
-  button.style.position = 'absolute';
-  button.style.right = '15px';
-  button.style.top = '15px';
-
-  button.type = 'button';
-  button.innerHTML = '<span class="material-icons-outlined">delete_outline</span>';
-
-  button.addEventListener('click', event => {
-    myLibrary.removeBook(bookNumber);
-    const shelf = article.parentNode;
-    shelf.removeChild(article);
-  });
-  return button;
-}
-
-function createArticle(book, bookNumber) {
-  let bookArticle = document.createElement('article');
-  bookArticle.classList.add('bookshelf__book');
-
-  const bookAttributeToDisplayName = {
-    title: 'Title:',
-    author: 'Author:',
-    pageCount: 'Pages:',
-    yearOfPublishing: 'Published:',
-    readStatus: 'Already read:',
-  };
-
-  for (attribute in bookAttributeToDisplayName) {
-    const bookData = document.createElement('div');
-    bookData.classList.add('book__info-entry');
-
-    const spanKey = document.createElement('span');
-    spanKey.style.width = '120px';
-    spanKey.textContent = bookAttributeToDisplayName[attribute];
-
-    const spanValue = document.createElement('span');
-    spanValue.textContent = book[attribute];
-
-    bookData.appendChild(spanKey);
-    bookData.appendChild(spanValue);
-
-    bookArticle.appendChild(bookData);
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.yearOfPublishing = yearOfPublishing;
+    this.readStatus = readStatus;
   }
 
-  const removeButton = createRemoveButton(bookNumber, bookArticle);
-  bookArticle.appendChild(removeButton);
+  get id() {
+    return this.#id;
+  }
 
-  return bookArticle;
+  toJSON() {
+    return {
+      title: this.title,
+      author: this.author,
+      pageCount: this.pageCount,
+      year: this.yearOfPublishing,
+      status: this.readStatus,
+    };
+  }
 }
 
-const buttonAddNew = document.querySelector('.header__button_type_add-new');
-/** Implements the 'New book' button feature. */
-buttonAddNew.addEventListener('click', event => {
-  const form = document.querySelector('.form');
-  form.classList.remove('form__disabled');
-});
-
-const buttonCancel = document.querySelector('.form-new-book__button_type_cancel');
-buttonCancel.addEventListener('click', event => {
-  const form = document.querySelector('.form');
-  form.classList.add('form__disabled');
-
-  // Cleanup input fields of the form
-  ['#title', '#author', '#page-count', '#year'].forEach(id => {
-    document.querySelector(id).value = '';
-  });
-  document.querySelector('#read-status').checked = false;
-});
-
-const buttonAccept = document.querySelector('.form-new-book__button_type_accept');
-/** The listener adds a new book to the library */
-buttonAccept.addEventListener('click', event => {
-  const args = ['#title', '#author', '#page-count', '#year'];
-  const book = new Book(
-    ...args.map(arg => document.querySelector(arg).value),
-    document.querySelector('#read-status').checked
-  );
-
-  // Put the book in the library.
-  const bookId = myLibrary.addBook(book);
-
-  // Add the book card to the shelf.
-  const article = createArticle(book, bookId);
-  bookshelf.appendChild(article);
-
-  // Cleanup input fields of the form
-  ['#title', '#author', '#page-count', '#year'].forEach(id => {
-    document.querySelector(id).value = '';
-  });
-  document.querySelector('#read-status').checked = false;
-});
-
-/** Initialize libary */
-let leviathanWakes = new Book('Leviathan Wakes', 'James S.A. Corey', 577, 2011, true);
-let calibansWar = new Book("Caliban's War", 'James S.A. Corey', 605, 2012, true);
-let abaddonsGate = new Book("Abaddon's Gate", 'James S.A. Corey', 547, 2013, false);
+let leviathanWakes = new Book(
+  'Leviathan Wakes',
+  'James S.A. Corey',
+  577,
+  2011,
+  true,
+);
+let calibansWar = new Book(
+  "Caliban's War",
+  'James S.A. Corey',
+  605,
+  2012,
+  true,
+);
+let abaddonsGate = new Book(
+  "Abaddon's Gate",
+  'James S.A. Corey',
+  547,
+  2013,
+  false,
+);
 let cibolaBurn = new Book('Cibola Burn', 'James S.A. Corey', 591, 2014, false);
-let nemesisGames = new Book('Nemesis Games', 'James S.A. Corey', 536, 2015, false);
-let babylonsAshes = new Book("Babylon's Ashes", 'James S.A. Corey', 544, 2016, false);
+let nemesisGames = new Book(
+  'Nemesis Games',
+  'James S.A. Corey',
+  536,
+  2015,
+  false,
+);
+let babylonsAshes = new Book(
+  "Babylon's Ashes",
+  'James S.A. Corey',
+  544,
+  2016,
+  false,
+);
 let persepolisRising = new Book(
   'Persepolis Rising',
   'James. S.A. Corey',
   560,
   2017,
-  false
+  false,
 );
-let tiamatsWrath = new Book("Tiamat's Wrath", 'James. S.A. Corey', 560, 2019, false);
+let tiamatsWrath = new Book(
+  "Tiamat's Wrath",
+  'James. S.A. Corey',
+  560,
+  2019,
+  false,
+);
+let leviathanFalls = new Book(
+  'Leviathan Falls',
+  'James S.A. Corey',
+  518,
+  2021,
+  false,
+);
+
+const libraryController = new LibraryController(
+  new LibraryView(),
+  new LibraryModel(),
+);
 
 [
   leviathanWakes,
-  abaddonsGate,
   calibansWar,
+  abaddonsGate,
   cibolaBurn,
   nemesisGames,
   babylonsAshes,
   persepolisRising,
   tiamatsWrath,
-].forEach(someBook => {
-  const bookId = myLibrary.addBook(someBook);
-  const article = createArticle(someBook, bookId);
-  bookshelf.appendChild(article);
+  leviathanFalls,
+].forEach((someBook) => {
+  libraryController.addBook(someBook);
 });
